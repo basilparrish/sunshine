33b4a4d32332c6971361ae33a7c92816b7cba185
dafloorisfake <157428890+dafloorisfake@users.noreply.github.com>
2025-03-19 22:08:32 -0700

    8

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -4,8 +4,7 @@ import './Home.css';
 function Home() {
   return (
     <div className="page">
-      <img src={`${process.env.PUBLIC_URL}/favicon.ico`} alt="Site Icon" className="site-icon" />
-      <h1>Welcome to the Domain of Mr. Parrish</h1>
+      <img src="/favicon.ico" alt="Site Icon" className="site-icon" />
       <p>This is the home page. Feel free to explore!</p>
       <div className="contact-info">
         <h2>Contact Info</h2>
